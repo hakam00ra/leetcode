401788b5916b51eb52912611757aae2e272fe374
hakam00ra <50047346+hakam00ra@users.noreply.github.com>
2022-12-09 11:53:50 +0200

    Update 0013-roman-to-integer.c

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,7 +1,7 @@
 
 
 int romanToInt(char * s){
-
+    int sum = 0;
     int value[100];
     value['I'] = 1;
     value['V'] = 5;
@@ -11,13 +11,12 @@ int romanToInt(char * s){
     value['D'] = 500;
     value['M'] = 1000;
     value['\0'] = 0;
-
-    int sum = 0;    
+        
     for (int i=0; s[i] != '\0'; i++) {
         if (value[s[i]] < value[s[i+1]])
-        sum -= value[s[i]];
+            sum -= value[s[i]];
         else
-        sum += value[s[i]];
+            sum += value[s[i]];
     }
     return sum;
 }
